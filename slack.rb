a7d728dcd44e13f8952a056c38996a9d65334087
f-teruhisa <teru_fukumoto@outlook.jp>
2019-08-09 01:58:55 +0900

    add env file and code

--- slack.rb
+++ slack.rb
@@ -1,9 +1,12 @@
 require 'slack-ruby-client'
+require 'dotenv'
 
-  Slack.configure do |conf|
-    conf.token = 'xoxb-37175438516-715733692496-dy45X6O4yRo23F9KW5lGXbjP'
-  end
+Dotenv.load
 
-  client = Slack::Web::Client.new
-  client.chat_postMessage(channel: '#connpass_notification', text: 'Hello World', as_user: true)
+Slack.configure do |conf|
+  conf.token = ENV['SLACK_API_TOKEN']
+end
+
+client = Slack::Web::Client.new
+client.chat_postMessage(channel: '#connpass_notification', text: 'Hello World', as_user: true)
 
