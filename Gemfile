ee814f31e92596bd7450c6ca65bbaf4fc1e311de
f-teruhisa <teru_fukumoto@outlook.jp>
2019-08-10 16:23:43 +0900

    add method create groups

--- Gemfile
+++ Gemfile
@@ -1,3 +1,6 @@
 source 'http://rubygems.org'
 
 gem 'async-websocket', '~> 0.8.0'
+gem 'slack-ruby-client'
+gem 'dotenv'
+gem 'csv'
--- script/slack.rb
+++ script/slack.rb
@@ -3,10 +3,24 @@ require 'dotenv'
 require 'csv'
 
 Dotenv.load
-members = []
+
+members, groups = [], []
+numbers_of_group = ENV['NUMBERS_OF_GROUP'].to_i
 
 CSV.foreach('data/members.csv', headers: true) do |member|
   members << member
+end
+
+numbers_of_group.times do
+  empty_group = Array.new
+  groups << empty_group
+end
+
+members.each_with_index do |member, i|
+  number = i % numbers_of_group
+  group = groups[number]
+  group << number
+  group << member
 end
 
 Slack.configure do |conf|
