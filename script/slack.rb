c4824f1dd5e150e9ea3906d86948b3599b48af4f
f-teruhisa <teru_fukumoto@outlook.jp>
2019-08-10 16:43:48 +0900

    refactorig ruby loginc with definating methods

--- script/slack.rb
+++ script/slack.rb
@@ -4,21 +4,37 @@ require 'csv'
 
 Dotenv.load
 
-members, groups = [], []
+groups = []
 numbers_of_group = ENV['NUMBERS_OF_GROUP'].to_i
 
-CSV.foreach('data/members.csv', headers: true) do |member|
-  members << member
+def read_csv(members)
+  members = []
+  CSV.foreach('data/members.csv', headers: true) do |member|
+    members << member
+  end
+  return members
 end
 
-numbers_of_group.times do
+def create_groups(numbers_of_group, groups, members)
+  numbers_of_group.times { create_group(groups) }
+  split_members(members, numbers_of_group, groups)
+end
+
+def create_group(groups)
   empty_group = Array.new
   groups << empty_group
 end
 
-members.each_with_index do |member, i|
-  number = i % numbers_of_group
-  group = groups[number]
+def split_members(members, numbers_of_group, groups)
+  members.each_with_index do |member, i|
+    number = i % numbers_of_group
+    group = groups[number]
+    assign_member_into_groups(group, number+1, member)
+    puts group
+  end
+end
+
+def assign_member_into_groups(group, number, member)
   group << number
   group << member
 end
@@ -26,6 +42,9 @@ end
 Slack.configure do |conf|
   conf.token = ENV['SLACK_API_TOKEN']
 end
+
+members = read_csv(members)
+create_groups(numbers_of_group, groups, members)
 
 client = Slack::Web::Client.new
 client.chat_postMessage(channel: '#connpass_notification', text: 'Hello World', as_user: true)
