e4f14a0ebe0c1d91c5212494bc8ef32f6b495b47
f-teruhisa <teru_fukumoto@outlook.jp>
2019-08-10 15:22:21 +0900

    add csv gem and read file

--- script/slack.rb
+++ script/slack.rb
@@ -1,7 +1,13 @@
 require 'slack-ruby-client'
 require 'dotenv'
+require 'csv'
 
 Dotenv.load
+members = []
+
+CSV.foreach('data/members.csv', headers: true) do |member|
+  members << member
+end
 
 Slack.configure do |conf|
   conf.token = ENV['SLACK_API_TOKEN']
@@ -9,4 +15,3 @@ end
 
 client = Slack::Web::Client.new
 client.chat_postMessage(channel: '#connpass_notification', text: 'Hello World', as_user: true)
-
